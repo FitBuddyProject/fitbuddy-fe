1ba0ab9e3fe2bec29a61e046b2105d89eb805069
Seungyeon Jeong <ssyeon612@gmail.com>
2024-07-03 10:16:57 +0900

    feat: 도넛 샘플 obj 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+
+const Object3D = () => {
+  const object = useLoader(OBJLoader, "models/donut.obj");
+
+  return <primitive object={object} />;
+};
+
 const Home = () => {
   return (
-    <>
+    <main style={{ height: "100vh" }}>
       <div>Home</div>
       <h1>Donut Sample</h1>
-    </>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Object3D />
+      </Canvas>
+    </main>
   );
 };
 
