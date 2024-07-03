90614a2cba666202ba40bd24f6ebfab3094ed5d8
Seungyeon Jeong <ssyeon612@gmail.com>
2024-07-03 10:00:11 +0900

    feat: css 초기화화

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import GlobalStyle from './styles/GlobalStyle'
 import Home from "./pages/Home";
+import Header from './components/Header'
 
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-
-export const GlobalStyle = createGlobalStyle`
-${reset}
-* {
-    box-sizing: border-box;
-  }
-  a {
-  color: inherit;
-  text-decoration: none;
-  }
-  li {
-  list-style: none;
-  }
-
-  button{
-    background: none;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
-  }
-`;
 
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Routes>
         <Route path="/home" element={<Home />}></Route>
       </Routes>
