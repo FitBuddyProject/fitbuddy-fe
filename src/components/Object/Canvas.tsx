4515e03ab0a2aded473226b0462c9c85fdfd850c
ssyeon612 <ssyeon612@gmai.com>
2024-07-11 23:35:44 +0900

    feat: 샘플 obj 파일 렌더링 테스트

---
+++ src/components/Object/Canvas.tsx
@@ -0,0 +1,24 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Html, OrbitControls, useProgress } from "@react-three/drei";
+
+import Model from "./Model";
+import LightController from "./LightController";
+
+function Loader() {
+    const { progress } = useProgress();
+    return <Html center>{progress} % loaded</Html>;
+}
+
+const CanvasComponent = () => {
+    return (
+        <Canvas>
+            <Suspense fallback={<Loader />}>
+                <OrbitControls />
+                <LightController />
+                <Model url="models/bunny.obj" color="hotpink" />
+            </Suspense>
+        </Canvas>
+    );
+};
+export default CanvasComponent;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import CanvasComponent from "../components/Object/Canvas";
+
+const Home = () => {
+    return (
+        <main style={{ height: "100vh" }}>
+            <CanvasComponent />
+        </main>
+    );
+};
+
+export default Home;
