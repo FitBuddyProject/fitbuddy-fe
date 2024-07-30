aa33cd90d09e6978b55893a995659b9e442be73f
heath <jannhyunsoo@gmail.com>
2024-07-31 00:36:47 +0900

    feat: login 작업 - login page2 작업

--- src/routes/RoutesGroup.tsx
+++ src/routes/RoutesGroup.tsx
@@ -5,6 +5,7 @@ import Home from "../pages/Home";
 import Intro from "pages/Intro";
 import LoginPage from "pages/LoginPage/LoginPage";
 import InfoPage from "pages/InfoPage";
+import LoginPage2 from "../pages/LoginPage/LoginPage2";
 
 const RoutesGroup = () => {
   return (
@@ -12,6 +13,8 @@ const RoutesGroup = () => {
       <Route path="/" element={<Home />}></Route>
       <Route path="/intro" element={<Intro />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/login2" element={<LoginPage2 />}></Route>
+      <Route path="/login3" element={<Home />}></Route>
       <Route path="/my-page" element={<MyPage />}></Route>
       <Route path="/info" element={<InfoPage />}></Route>
     </Routes>
