ca3683d03295a0333ca6ff797db517efb6a49b9d
ssyeon612 <ssyeon612@gmai.com>
2024-08-02 22:12:45 +0900

    feat: 행동기록 페이지 수정

--- src/store/root/rootReducer.ts
+++ src/store/root/rootReducer.ts
@@ -2,13 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authSlice from "../slices/auth/auth.slice";
 import headerSlice from "../slices/header";
 import modalSlice from "../slices/modal";
-import historySlice from "store/slices/history/history.slice";
 
 const rootReducer = combineReducers({
-  auth: authSlice,
-  header: headerSlice,
-  modal: modalSlice,
-  history: historySlice
+    auth: authSlice,
+    header: headerSlice,
+    modal: modalSlice,
 });
 
 export default rootReducer;
