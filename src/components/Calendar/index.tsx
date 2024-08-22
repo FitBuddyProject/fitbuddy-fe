735f660b3cf5a27787115fea3ed0f9778eb22a86
Seungyeon Jeong <ssyeon612@gmail.com>
2024-08-22 17:02:39 +0900

    style: 달력 스타일 변경

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,93 +1,164 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import styled from "styled-components";
 import "./calendar.css";
+import dayjs from "dayjs";
+import styled from "styled-components";
+import { theme } from "styles/theme";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const CustomCalendar = () => {
-    const [value, onChange] = useState<Value>(new Date());
+const Box = styled.div`
+  border-radius: 0.4rem;
+  padding: 0 4rem;
+  background-color: ${({ theme }) => theme.color.blueLight};
+  span {
+    color: ${({ theme }) => theme.color.blue};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    font-size: ${({ theme }) => theme.fontSize.xs};
+  }
+`;
 
-    return (
-        <StyledCalendar
-            onChange={onChange}
-            value={value}
-            calendarType="gregory"
-            view="month"
-            prevLabel={null}
-            nextLabel={null}
-            prev2Label={null}
-            next2Label={null}
-            showNeighboringMonth={false}
-            tileContent={"O"}
-        />
-    );
+const CustomCalendar = () => {
+  const today = new Date();
+  const [value, onChange] = useState<Value>(today);
+
+  // 일요일(0) 또는 토요일(6)인지 확인하여 클래스를 지정합니다.
+  const tileClassName = ({ date }: any) => {
+    // 일요일
+    if (date.getDay() === 0) return "sunday";
+
+    // 토요일
+    if (date.getDay() === 6) return "saturday";
+
+    return "";
+  };
+
+  // 운동한날 달력에 표기
+  const dayList = ["2024-08-10", "2024-08-21", "2024-08-02", "2024-08-14", "2024-08-27"];
+  const addContent = ({ date }: any) => {
+    const contents = [];
+    if (dayList.find((day) => day === dayjs(date).format("YYYY-MM-DD"))) {
+      contents.push(
+        <Box>
+          <span>O</span>
+        </Box>
+      );
+    }
+
+    return <div>{contents}</div>;
+  };
+
+  const onClickDay = () => {
+    console.log("onClickDay");
+  };
+
+  return (
+    <StyledCalendar
+      calendarType="gregory"
+      view="month"
+      value={value}
+      formatDay={(locale, date) => dayjs(date).format("DD")}
+      tileClassName={tileClassName}
+      onChange={onChange}
+      onClickDay={onClickDay}
+      tileContent={addContent}
+      showNeighboringMonth={false}
+      prevLabel={null}
+      nextLabel={null}
+      prev2Label={null}
+      next2Label={null}
+    />
+  );
 };
 
 export default CustomCalendar;
 
 const StyledCalendar = styled(Calendar)`
-    border: 0;
+  border: 0;
 
-    /* 날짜의 글자 스타일 */
-    .react-calendar__month-view__days__day-names,
-    .react-calendar__month-view__days__day {
-        font-size: ${({ theme }) => theme.fontSize.xs};
-        line-height: 120%;
-        font-weight: 400;
-    }
+  /* 날짜 선택  */
+  .react-calendar__navigation {
+    display: none;
+  }
 
-    /* 날짜 간 간격 */
-    .react-calendar__month-view__days {
-        button {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-    }
+  /* 날짜의 글자 스타일 */
+  .react-calendar__month-view__days__day-names,
+  .react-calendar__month-view__days__day {
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    line-height: 120%;
+    font-weight: 400;
+  }
 
-    /* 선택된 날짜 타일 */
-    .react-calendar__tile--active {
-        color: ${({ theme }) => theme.color.white};
-        background-color: ${({ theme }) => theme.color.primary};
-    }
+  /* 요일 스타일 */
+  .react-calendar__month-view__weekdays abbr {
+    font-size: ${({ theme }) => theme.fontSize.xs};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+    color: ${({ theme }) => theme.color.blueGrey70};
+  }
 
-    /* 호버 및 액티브 스타일 */
-    .react-calendar__tile--active:active,
-    .react-calendar__tile:focus,
-    .react-calendar__tile:hover {
+  /* 주말 요일 스타일 */
+  .react-calendar__month-view__weekdays__weekday {
+    & abbr[aria-label="일요일"] {
+      color: ${({ theme }) => theme.color.error};
     }
+    & abbr[aria-label="토요일"] {
+      color: ${({ theme }) => theme.color.blue};
+    }
+  }
 
-    /* 오늘 날짜에 대한 호버 및 액티브 스타일 */
-    .react-calendar__tile--now:active,
-    .react-calendar__tile--now:hover {
+  /* 날짜 간 간격 */
+  .react-calendar__month-view__days {
+    button {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
+  }
 
-    /* 오늘 날짜 스타일 */
-    .react-calendar__tile--now {
-        color: ${({ theme }) => theme.color.primary};
-        background-color: ${({ theme }) => theme.color.primaryLight};
-        border-radius: 50%;
-    }
+  /* 호버 및 액티브 스타일 */
+  .react-calendar__tile--active:active,
+  .react-calendar__tile:focus,
+  .react-calendar__tile:hover {
+    background: transparent;
+  }
 
-    /* 요일 스타일 */
-    .react-calendar__month-view__weekdays abbr {
-        font-size: ${({ theme }) => theme.fontSize.xs};
-        font-weight: ${({ theme }) => theme.fontWeight.medium};
-        color: ${({ theme }) => theme.color.blueGrey70};
-    }
+  /* 토요일 날짜 스타일 */
+  .react-calendar__tile.saturday abbr {
+    color: ${({ theme }) => theme.color.blue};
+  }
 
-    .react-calendar__month-view__weekdays__weekday--weekend abbr {
-        color: "green";
-        background-color: "skyblue";
+  /* 일요일 날짜 스타일 */
+  .react-calendar__tile.sunday abbr {
+    color: ${({ theme }) => theme.color.error};
+  }
+    
+  /* 선택된 날짜 타일 */
+  .react-calendar__tile--active,
+  .react-calendar__tile:hover {
+    background: transparent;
+    abbr {
+      color: ${({ theme }) => theme.color.white};
+      background-color: ${({ theme }) => theme.color.primary};
+      border-radius: 50%;
+      width: 1.5rem;
+      height: 1.5rem;
+      line-hight: 1.5rem;
     }
+  }
 
-    /* 토요일 날짜 스타일 */
-    .react-calendar__month-view__days__day:not(.react-calendar__month-view__days__day--weekend)
-        + .react-calendar__month-view__days__day--weekend:not(.react-calendar__month-view__days__day--neighboringMonth) {
-        color: ${({ theme }) => theme.color.blue};
+  /* 오늘 날짜 스타일 */
+  .react-calendar__tile--now {
+    background: transparent;
+    abbr {
+      background-color: ${({ theme }) => theme.color.primaryLight};
+      color: ${({ theme }) => theme.color.primary};
+      border-radius: 50%;
+      width: 1.5rem;
+      height: 1.5rem;
+      line-hight: 1.5rem;
     }
+  }
 `;
