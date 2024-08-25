1e1205ef90022efb483987631bbf2694f6d76a64
heath <jannhyunsoo@gmail.com>
2024-08-25 23:34:07 +0900

    feat: 개인정보-기본창

--- src/pages/MyInfo/index.tsx
+++ src/pages/MyInfo/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { headerActions } from "../../store/slices/header";
 import { MyInfoContainer, MyInfoWrapper, MyInfoBodySect, MyInfoFooterSect, MyInfoHeaderSect } from "./MyInfo.styles";
@@ -14,17 +14,45 @@ const MyInfo = () => {
         <main>
             <MyInfoWrapper>
                 <MyInfoContainer>
-                    <MyInfoHeaderSect>달라는 사자</MyInfoHeaderSect>
-                    <MyInfoBodySect>휴대폰</MyInfoBodySect>
+                    <MyInfoHeaderSect>
+                        <div>
+                            <p>
+                                달리는 사자
+                            </p>
+                        </div>
+                        <Button>
+                            <p> 회원탈퇴</p>
+                        </Button>
+                    </MyInfoHeaderSect>
+
+                    <MyInfoBodySect>
+                        <div className="input-sect">
+                            <p>휴대폰 인증</p>
+                            <input type="text"/>
+                        </div>
+                        <div className="input-sect">
+                            <p>이메일 주소</p>
+                            <input type="text"/>
+                        </div>
+
+                        <div className="hint-sect">
+                            <p>이메일 주소를 등록해주세요.</p>
+                            <p>휴대폰 번호가 바뀌었을 때 로그인 수단으로 이용할 수 있어요.</p>
+                        </div>
+
+                    </MyInfoBodySect>
+
+
                     <MyInfoFooterSect>
-                        <Button type="button" color="primary" size="large" onClick={()=>{}}>
+                        <Button type="button" color="primary" size="large" onClick={() => {
+                        }}>
                             정보수정하기
                         </Button>
                     </MyInfoFooterSect>
                 </MyInfoContainer>
             </MyInfoWrapper>
         </main>
-    )
-}
+    );
+};
 
-export default MyInfo
+export default MyInfo;
