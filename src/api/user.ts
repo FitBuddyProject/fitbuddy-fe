7dee963da44c174651fa8cfc11562e712d96e28f
Seungyeon Jeong <ssyeon612@gmail.com>
2024-09-29 14:23:01 +0900

    fix: api 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,37 +2,35 @@
  * @description call user api
  */
 
+import axios from "axios";
 import api from "./api";
 
 const PREFIX = "/v1/user";
 
-export const sendable  = async (payload: any) => {
-    return await api.patch(`${PREFIX}/sendable`)
-}
+export const sendable = async (payload: any) => {
+  return await api.patch(`${PREFIX}/sendable`);
+};
 
-export const signin  = async (payload: any) => {
-    return await api.patch(`${PREFIX}/sign/in`)
-}
+export const signin = async (payload: any) => {
+  return await api.patch(`${PREFIX}/sign/in`, { phone: "01011112222" });
+};
 
+export const signout = async (payload: any) => {
+  return await api.patch(`${PREFIX}/sign/out`);
+};
 
-export const signout  = async (payload: any) => {
-    return await api.patch(`${PREFIX}/sign/out`)
-}
+export const signup = async (payload: any) => {
+  return await api.post(`${PREFIX}/sign/up`);
+};
 
+export const syncPushToken = async (payload: any) => {
+  return await api.patch(`${PREFIX}/sync/push-token`);
+};
 
-export const signup  = async (payload: any) => {
-    return await api.post(`${PREFIX}/sign/up`)
-}
+export const syncTired = async (payload: any) => {
+  return await api.patch(`${PREFIX}/sync/tired`);
+};
 
-
-export const syncPushToken  = async (payload: any) => {
-    return await api.patch(`${PREFIX}/sync/push-token`)
-}
-
-export const syncTired  = async (payload: any) => {
-    return await api.patch(`${PREFIX}/sync/tired`)
-}
-
-export const verifyPhone  = async (payload: any) => {
-    return await api.get(`${PREFIX}/verify/phone/${payload.phone}`)
-}
+export const verifyPhone = async (payload: any) => {
+  return await api.get(`${PREFIX}/verify/phone/${payload.phone}`);
+};
