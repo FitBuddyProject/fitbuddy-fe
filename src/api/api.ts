3441d0bce91288456ee3da5a125a5b4dd84ac31e
Seungyeon Jeong <ssyeon612@gmail.com>
2024-10-11 16:07:26 +0900

    캐릭터 회선 제한 및 헤더에 auth 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,9 +4,15 @@ const api = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
-    Authorization:
-      "eyJ0b2tlblR5cGUiOiJBQ0NFU1MiLCJhbGciOiJIUzI1NiJ9.eyJCT0RZIjoie1widXVpZFwiOlwiYmJlZDA4MzFmOTMwNzczNTEzZDY4Yjk4XCIsXCJpZFwiOlwiMDEwMTExMTIyMjJcIixcImxhc3RNb2RpZmllZERhdGVcIjpcIjIwMjQtMDktMTdUMTk6NTU6MTkuNjg0XCIsXCJqb2luRGF0ZVwiOlwiMjAyNC0wOS0xN1QxOTo1NToxOS42ODRcIn0iLCJpc3MiOiJmaXRCdWRkeSIsImp0aSI6ImJiZWQwODMxZjkzMDc3MzUxM2Q2OGI5OCIsImV4cCI6MTcyODEzNzMyNywibmJmIjoxNzI4MTM3MzI3LCJpYXQiOjE3Mjg3NDIxMjd9.qg0-p8LZcyBDKiYKbl-ovoxY76IkpYjn91TuoDh27Is",
   },
 });
+
+export const setAuthorizationHeader = (token: string) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
 
 export default api;
--- src/components/BuddyComponent/controls/CustomOrbitControls.jsx
+++ src/components/BuddyComponent/controls/CustomOrbitControls.jsx
@@ -2,19 +2,19 @@ import { useRef } from "react";
 import { OrbitControls } from "@react-three/drei";
 
 const CustomOrbitControls = () => {
-    const controlsRef = useRef();
+  const controlsRef = useRef();
 
-    return (
-        <OrbitControls
-            ref={controlsRef}
-            enableZoom={false}
-            enablePan={false}
-            enableRotate={true}
-            minPolarAngle={Math.PI / 2} // 회전 제한
-            maxPolarAngle={Math.PI / 2} // 회전 제한
-            minDistance={10}
-        />
-    );
+  return (
+    <OrbitControls
+      ref={controlsRef}
+      enableZoom={false}
+      enablePan={false}
+      enableRotate={false} // 회전 비활성화
+      minPolarAngle={Math.PI / 2}
+      maxPolarAngle={Math.PI / 2}
+      minDistance={10}
+    />
+  );
 };
 
 export default CustomOrbitControls;
